1fc5d458f42d50026b18e9fd17a199ef1f5c412f
shannonli7 <lishann@umich.edu>
2020-10-16 21:37:56 -0400

    Code changes

--- js/weather.js
+++ js/weather.js
@@ -1,21 +1,28 @@
 function gettingJSON(){
     //Display the forecast
     // Your code here.
-    let weather_app_url = "https://api.openweathermap.org/data/2.5/weather";
+    const appid = "c8f5d2484d67510fbd2a720a57f1e04a";
+    const weather_app_url = "https://api.openweathermap.org/data/2.5/weather?";
+    const api_end = '&appid='+ appid;
 
-    // "country code" -> sys.country
-    // "city code" -> id
-    const dict = {
+    let dict = {
       zip: "zip code",
-      appid: "c8f5d2484d67510fbd2a720a57f1e04a"
     }
-    weather_app_url += '?q=London&appid='+ dict.appid;
+
+    let url = weather_app_url + 'q=Ann+Arbor'+ api_end;
+    let place = document.getElementById("location").value;
+    if(place !== ""){
+      // replace all spaces in the location with a + (google this)
+      // for a city, the prefix is a q=
+      // for a zipcode the prefix is a zip=
+      url = weather_app_url + place + api_end;
+      console.log(url);
+    }
     //Set default location if one isn't provided
     let location;
-    // location = "Ann Arbor";
-    // Your code here.
 
-    fetch(weather_app_url, {
+
+    fetch(url, {
       mode: 'cors',
       credentials: "same-origin"
     })
